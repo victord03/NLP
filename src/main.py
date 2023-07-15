b15b780b777b6fe194cdf80d479fa273a126dfba
Your Name <vkaklam@dome.imgln.net>
2023-07-15 19:40:57 +0300

    Added basic parsing functions.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,57 @@
-from input_output.io_ops import open_file
+
+def remove_punctuation(text: str) -> str:
+    """Removed all provided punctuation from the text."""
+
+    punctuation_to_be_removed = ['!', ',', '-', '\'', '\"', '[', ']', '?', '.', '(', ')']
+
+    new_text = str()
+
+    for char in text:
+        if char not in punctuation_to_be_removed:
+            new_text += char
+
+    return new_text
+
+
+def remove_semantics(text: str) -> str:
+    """Removed all provided punctuation from the text."""
+
+    words_to_be_removed = ['while', 'and', 'the', 'to', 'it', 'for', 'by', 'on', 'in', 'as']
+
+    new_text = list()
+
+    for word in text.split(' '):
+        if word.lower() not in words_to_be_removed:
+            new_text.append(word)
+
+    return ' '.join(new_text)
+
+
+def test_remove_empty_spaces(text: str) -> str:
+    new_text = str()
+
+    for char in text:
+        if not char.isspace():
+            new_text += char
+
+    return new_text
+
+
+def words_into_list(text: str) -> list:
+
+    new_text = str()
+
+    for word in text:
+        if word:
+            new_text.append(word)
+
+    return text.split(' ')
+
+
 
 
 def main():
-    """Main Func"""
-
-    file_path = r'C:\Users\3tr0001\Desktop\nlp.txt'
-
-    text = open_file(file_path)
-    print(text)
-
-    # text_as_lists = text.split("\n")
-    # print(text_as_lists)
+    ...
 
 
 if __name__ == "__main__":
---
+++ test/test_main.py
@@ -0,0 +1,32 @@
+from src import main
+
+
+def test_remove_punctuation():
+
+    text = 'While punctuation is great, it is important - imperative - to remove it for sentiment analysis!'
+    expected = 'While punctuation is great it is important  imperative  to remove it for sentiment analysis'
+
+    assert main.remove_punctuation(text) == expected
+
+
+def test_remove_semantics():
+    text = 'While punctuation is great it is important  imperative  to remove it for sentiment analysis'
+    expected = 'punctuation is great is important  imperative  remove sentiment analysis'
+
+    assert main.remove_semantics(text) == expected
+
+
+def test_remove_empty_spaces():
+
+    text = 'While punctuation is great it is important  imperative  to remove it for sentiment analysis'
+    expected = 'punctuation is great is important imperative remove sentiment analysis'
+
+    assert main.test_remove_empty_spaces(text) == expected
+
+
+def test_words_into_list():
+
+    text = 'punctuation is great is important  imperative  remove sentiment analysis'
+    expected = ['punctuation', 'is', 'great', 'is', 'important', 'imperative', 'remove', 'sentiment', 'analysis']
+
+    assert main.words_into_list(text) == expected
